0b4c0386c8a3d05e9a80a8e919d9305b674c37c1
Chigozie Nkwocha <bueze25@gmail.com>
2024-09-16 15:03:40 +0300

    Add files via upload

---
+++ Biology/get_illumina_gene_ids.R
@@ -0,0 +1,44 @@
+## R code obtains GPL data (annotated or not) of given platform ID for
+## each Geo ID and Writes to a file
+
+library(GEOquery)
+library(stringr)
+
+
+# load series and platform data from GEO
+# "GPL10558", "GPL21572", "GPL17692"
+fetch_gene_ids <- function(gpl_id, probe_ids) {
+  gpl <- getGEO(GEO = gpl_id, AnnotGPL = TRUE)
+
+  # extract data tables
+  gpl_data <- Table(gpl)
+  # get gene info of probe IDs
+  gene_info <- gpl_data[gpl_data$ID %in% probe_ids, ]
+  return(gene_info)
+}
+
+files <- list.files(path = "./microarray",
+                    pattern = "*counts*.csv",
+                    full.names = TRUE)
+
+
+# GPL IDs for the datasets
+gpl_ids <- c("GPL10558", "GPL21572", "GPL17692")
+
+print(files)
+
+
+for (i in seq(1, length(gpl_ids))){
+  # get file, GPL_ID and file basename
+  file <- files[i]
+  gpl_id <- gpl_ids[i]
+  file_id <- stringr::str_extract(basename(file), "^GSE\\d+")
+
+  # read file and fetch gene IDs of illumina probeset IDs
+  df <- read.csv(file)
+  illumina_ids <- as.vector(df[, 1])
+  gene_ids <- fetch_gene_ids(gpl_id, illumina_ids)
+
+  write.csv(gene_ids, paste0(file_id, "_", gpl_id, "_gene_ids.csv"),
+            row.names = FALSE)
+}
